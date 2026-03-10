7612042e7a51f142db4086406ff695f733face16
Marko Urbanc <urbancmarko1@gmail.com>
2026-03-10 16:13:35 +0000

    feat: :zap: improve previous plot

--- Code/ipw-plot.py
+++ Code/ipw-plot.py
@@ -18,7 +18,7 @@ psi_0 = 0.0
 psi_prime_0 = 1.0
 psi_init = np.array([psi_0, psi_prime_0])
 
-step_size = 1/200000
+step_size = 1/10000
 upper = 500
 
 ts = time()
@@ -27,18 +27,29 @@ te = time()
 print(f"Time taken: {te - ts:.2f} seconds")
 print(f"Found energy levels: {ipw_E}")
 
-fig, ax = plt.subplots(figsize=(8, 5))
+fig, ax = plt.subplots(1, 2, figsize=(12, 6))
 colors = cmr.take_cmap_colors("cmr.tropical", len(ipw_E), cmap_range=(0.0, 0.8))
 for i, psi in enumerate(ipw_psi):
-    ax.plot(ipw_x, psi, label=f"$E_{i}={ipw_E[i]:.2f}$", color=colors[i])
-ax.set_xlabel('x')
-ax.set_ylabel('$\psi(x)$')
-ax.legend(ncols=2, loc="lower left", fontsize=10)
-plt.suptitle(f"Particle in a Box aka Infinite Potential Well")
-par_str = f"Evaluated with PEFRL method, $\log_{{10}}(g)={int(np.log10(1/step_size))}$, $E_{{max}}={upper}$"
-plt.title(par_str, fontsize=10)
+    ax[0].plot(ipw_x, psi, label=f"$E_{i}={ipw_E[i]:.2f}$", color=colors[i])
+ax[0].set_xlabel('x')
+ax[0].set_ylabel('$\psi(x)$')
+ax[0].set_title("Numerically Evaluated Wavefunctions")
+
+for i, (psi, ana) in enumerate(zip(ipw_psi, ipw_ana)):
+    ax[1].plot(ipw_x, aerr(ana, psi), label=f"$E_{i}={ipw_E[i]:.2f}$", color=colors[i])
+ax[1].set_xlabel('x')
+ax[1].set_ylabel('Absolute Error $| \psi_{ana} - \psi_{num} |$')
+ax[1].set_yscale('log')
+ax[1].set_title("Abs. Err. Compared to Analytical Solution")
+
+handles, labels = ax[0].get_legend_handles_labels()
+fig.legend(handles, labels, loc="lower center", ncol=7, frameon=False, prop={"size": 10, "weight": "medium"}, 
+           title="Eigenenergies", title_fontproperties={"size": 12, "weight": "bold"})
+
+plt.suptitle(f"Eigen-functions and Energies for an Infinite Potential Well")
 plt.tight_layout()
-plt.subplots_adjust(top=0.87)
+par_str = f"Evaluated via Shooting with PEFRL method, $\log_{{10}}(g)={int(np.log10(1/step_size))}$, $E_{{max}}={upper}$"
+plt.subplots_adjust(top=0.825, bottom=0.15, right=0.97)
+plt.text(0.5, 0.93, par_str, ha="center", va="top", transform=plt.gcf().transFigure, fontsize=10, weight="medium")
 plt.savefig(f"./Images/particle_in_box_infinite_upper{upper}_step{step_size}.png", dpi=450)
 plt.show()
-
